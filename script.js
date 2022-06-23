386baa8cb4c84edb7a1d52bf3ac766f99cd3b90f
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-06-23 10:26:13 -0400

    Fixed issue with handleExponents function where operations were handled out of order.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-/* ******************************** //
-//     OPERATION FUNCTIONS          //
-// ******************************** */
-
-const modulus = (a, b) => {
-    return a % b;
-}
+/* ************************************ //
+//         OPERATION FUNCTIONS          //
+// ************************************ */
 
 const operate = (operator, value1, value2) => {
     const a = Number(value1), b = Number(value2);
@@ -20,9 +16,6 @@ const operate = (operator, value1, value2) => {
             break;
         case "/":
             return (a/b).toFixed(2);
-            break;
-        case "%":
-            return a % b;
             break;
         case "^":
             return Math.pow(a, b);
@@ -45,19 +38,16 @@ const operate = (operator, value1, value2) => {
 
 const handleParenthesis = (expression) => {
     let expressionStr = `${expression}`.replace(/[\(]/g, " (").replace(/[\)]/g, ") ");
-    console.log(expressionStr);
     const inputArr = expressionStr
         .split(' ')
         .map((element) => {
             // If a complete expression contained within () exists, strip the (), evaluate the expression and replace the () expression with its result in inputArr
             if(element.match(/^\(.*\)$/)) {
                 element = `${element}`.substring(1, element.length-1);
-                console.log(element);
                 return calculate(element);
             } else return element;
         });
     expressionStr = inputArr.join('');
-    console.log(expressionStr);
     // If a set of outer () exists after inner () are evaluated, handle those ()
     if(expressionStr.includes('(')) {
         expressionStr = handleParenthesis(expressionStr);
@@ -67,27 +57,32 @@ const handleParenthesis = (expression) => {
 }
 
 const handleExponents = (expression) => {
-    // Order of powers is reversed so that if there are stacked exponents (power to a power), the top level exponent will be handled first: 2^3^4 will be handled as 2^(3^4).
-    let expressionStr = `${expression}`.split('^').reverse().join('^').replace(/[\d]+\^[\d]+/, (x) => {
-        return ` ${x} `
-    });
-    console.log(expressionStr);
-    const inputArr = expressionStr
-        .split(' ')
-        .map((element) => {
-            // If a number ([\d]+) is raised to the power of another number using ^, the power is evaluated and returned to the inputArr
-            if(`${element}`.match(/^[\d]+\^[\d]+$/)) {
-                const expArr = `${element}`.split('^');
-                element = operate('^', expArr[1], expArr[0]);
-                console.log(element);
-                return element;
-            } else return element;
+    let expressionStr = '';
+    // Validate that only digits, ^, *, /, +, and - are used in expression
+    if(expression.match(/[^-\d\+\/\*\^]/g)) { // Invalid
+        console.log(`Error: Invalid input in handleExponents function: ${expression}`);
+    } else if(!expression.match(/[^\d\^]/g)) { // If only digits and ^ are present
+        // Order of multiple consecutive powers is reversed so that if there are stacked exponents (power to a power), the top level exponent will be handled first: 2^3^4 will be handled as 2^(3^4).
+        expressionStr = `${expression}`.split('^').reverse().join('^').replace(/[\d]+\^[\d]+/, (x) => {
+            return ` ${x} `
         });
-    expressionStr = inputArr.join('').split('^').reverse().join('^');
-    console.log(expressionStr);
+        const inputArr = expressionStr
+            .split(' ')
+            .map((element) => {
+                if(`${element}`.match(/^[\d]+\^[\d]+$/)) {
+                    const expArr = `${element}`.split('^');
+                    element = operate('^', expArr[1], expArr[0]);
+                    return element;
+                } else return element;
+            });
+        expressionStr = inputArr.join('').split('^').reverse().join('^');
+    } else {
+        // Store *, /, +, - operators and replace them once we handle the ^.
+        expressionStr = tempRemoveOperators(/[-\+\*\/]/g, expression);
+    }
     // If another power exists, recursively call handleExponents
-    if(expressionStr.includes('^')) {
-        expressionStr = handleExponents(expressionStr);
+    if(`${expressionStr}`.includes('^')) {
+        expressionStr = handleExponents(`${expressionStr}`);
     }
     // Return once all exponents have been handled
     return `${expressionStr}`;
@@ -96,43 +91,22 @@ const handleExponents = (expression) => {
 const handleMultiplicationDivision = (expression) => {
     let result = '';
     // Validate that only digits, *, /, +, and - are used in expression
-    if(expression.match(/[^-\d\+\/\*]/g)) {
-        // Invalid
+    if(expression.match(/[^-\d\+\/\*]/g)) { //Invalid
         console.log(`Error: Invalid input in handleMultiplicationDivision function: ${expression}`);
-    } else if(!expression.match(/[^\d\/\*]/g)) {
+    } else if(!expression.match(/[^\d\/\*]/g)) { // If only digits and *, / are present
         // Multiplication and division are handled in order from left to right.
-        let expressionStr = `${expression}`.replace(/\*/g, " * ").replace(/\//g, " / ");
-        console.log(expressionStr);
+        let expressionStr = `${expression}`.replace(/\*\//g, (x) => {
+            return ` ${x} `;
+        });
         const inputArr = expressionStr.split(' ');
-        console.log(inputArr);
         result = `${inputArr[0]}`;
-        console.log(result);
         for(let i=1; i<inputArr.length; i++) {
             result = operate(`${inputArr[i]}`, result, `${inputArr[i+1]}`);
             i++;
-            console.log(result);
         }
     } else {
-        // Store addition and subtraction operators so that we can replace them once we handle the mult and div
-        const addSubtOperatorArr = expression.match(/[-\+]/g);
-        console.log(addSubtOperatorArr);
-        // Temporarily remove +, - operators
-        let expressionStr = `${expression}`
-            .replace(/\+/g, ' ')
-            .replace(/-/g, ' ');
-        console.log(expressionStr);
-        // Each element is a digit or a series of *,/, and digits to be evaluated
-        const inputArr = expressionStr
-            .split(' ')
-            .map((element, index) => {
-                if(index<addSubtOperatorArr.length) {
-                    return `${calculate(element)}${addSubtOperatorArr[index]}`;
-                } else {
-                    return `${calculate(element)}`;
-                }
-        });
-        result = inputArr.join('');
-        console.log(result);
+        // Store +, - operators and replace them once we handle the * and /.
+        result = tempRemoveOperators(/[-\+]/g, expression);
     }
     return `${result}`;
 }
@@ -140,25 +114,49 @@ const handleMultiplicationDivision = (expression) => {
 const handleAdditionSubtraction = (expression) => {
     let result = '';
     // Validate that only digits, +, and - are used in expression
-    if(expression.match(/[^-\d\+\.]/g)) {
-        // Invalid
+    if(expression.match(/[^-\d\+\.]/g)) { //Invalid
         console.log(`Error: Invalid input in handleAdditionSubtraction function: ${expression}`);
     } else {
         // Addition and subtraction are handled in order from left to right.
-        let expressionStr = `${expression}`.replace(/\+/g, " + ").replace(/-/g, " - ");
-        console.log(expressionStr);
+        let expressionStr = `${expression}`.replace(/-\+/g, (x) => {
+            return ` ${x} `;
+        });
         const inputArr = expressionStr.split(' ');
-        console.log(inputArr);
         result = `${inputArr[0]}`;
-        console.log(result);
         for(let i=1; i<inputArr.length; i++) {
             result = operate(`${inputArr[i]}`, result, `${inputArr[i+1]}`);
             i++;
-            console.log(result);
         }
     }
     return `${result}`;
 }
+
+/* ************************************ //
+// TEMPORARILY REMOVE OPERATORS FUNCTION//
+// ************************************ */
+
+const tempRemoveOperators = (untargetedOperRegex, expression) => {
+    // Store untargeted operators so that we can replace them once we handle the target operator.
+    const operatorArr = expression.match(untargetedOperRegex);
+    // Temporarily remove untargeted operators operators
+    let expressionStr = `${expression}`
+        .replace(untargetedOperRegex, ' ');
+    // Each element is now a digit or a series of target operators and digits to be evaluated
+    const inputArr = expressionStr
+        .split(' ')
+        .map((element, index) => {
+            if(index<operatorArr.length) {
+                return `${calculate(element)}${operatorArr[index]}`;
+            } else {
+                return `${calculate(element)}`;
+            }
+    });
+    return `${inputArr.join('')}`;
+}
+
+/* ************************************ //
+//         CALCULATE FUNCTION           //
+// ************************************ */
 
 const calculate = (expression) => {
     let expressionStr = `${expression}`;
@@ -179,18 +177,21 @@ const calculate = (expression) => {
     }
 
     // handle Addition and Subtraction (in order from left to right)
-    if(expressionStr.includes('+') || expressionStr.includes('-')) {
+    if((expressionStr.includes('+') || expressionStr.includes('-')) && !expressionStr.includes('e')) {
         expressionStr = handleAdditionSubtraction(expressionStr);
     }
     
     return expressionStr;
 }
 
+/* ************************************ //
+//  INITIAL INPUT VALIDATION FUNCTIONS  //
+// ************************************ */
+
 const isValidExpression = (expression) => {
-    console.log(expression);
-    console.log(isValidInput(expression));
-    console.log(isValidParenthesis(expression));
-    console.log(isValidOperators(expression));
+    //console.log(isValidInput(expression));
+    //console.log(isValidParenthesis(expression));
+    //console.log(isValidOperators(expression));
     return isValidInput(expression)[0] && isValidParenthesis(expression)[0] && isValidOperators(expression)[0];
 }
 
@@ -241,6 +242,9 @@ const isValidOperators = (expression) => {
     return [true, ''];
 }
 
+/* ************************************ //
+//        INPUT ENTRY FUNCTIONS         //
+// ************************************ */ 
 const enterInput = (event) => {
     const inputContainer = document.getElementById("show_input");
     inputContainer.innerHTML += `${event.target.value}`;
@@ -267,7 +271,12 @@ const enterEquals = () => {
             result = calculate(expression);
         }
     }
-    inputContainer.innerHTML += `\r${result}`;
+    const outputContainer = document.getElementById("output_screen");
+    const output = document.createElement('div');
+    output.id = "output";
+    output.className = "output";
+    output.innerHTML = `${result}`;
+    outputContainer.appendChild(output);
 }
 
 const enterClear = (event) => {
@@ -279,6 +288,10 @@ const enterDelete = () => {
     const inputContainer = document.getElementById("show_input");
     inputContainer.innerHTML = `${inputContainer.innerHTML.substring(0,inputContainer.innerHTML.length-1)}`;
 }
+
+/* ************************************ //
+//        EVENT LISTENER FUNCTION       //
+// ************************************ */
 
 const addListeners = () => {
     const inputs = document.querySelectorAll(".input");
