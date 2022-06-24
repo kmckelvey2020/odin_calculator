a48771098e874b15aa874ba983b8b232ba863c31
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-06-24 12:54:49 -0400

    Passing all but one test in jest due to compounded rounding. Attempted to divide script.js into multiple files, but had trouble importing calculate function from a different file due to lodash dependecies. Not sure how to fix this yet so reverted to working single file version. Will have to reinstall jest for more testing.

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+
+/* ************************************ //
+//         CALCULATE FUNCTION           //
+// ************************************ */
+
+const calculate = (expression) => {
+    expression = implicitToExplicitMultiplication(expression);
+    expression = prepNegatives(expression);
+    let expressionStr = `${expression}`;
+
+    // handle Parentheses
+    if(expressionStr.includes('(')) {
+        expressionStr = handleParenthesis(expressionStr);
+    }
+
+    // handle Factorials
+    if(expressionStr.includes('!')) {
+        expressionStr = handleFactorials(expressionStr);
+    }
+
+    // handle Exponents
+    if(expressionStr.includes('^')) {
+        expressionStr = handleExponents(expressionStr);
+    }
+
+    // handle Multiplication and Division (in order from left to right)
+    if(expressionStr.includes('*') || expressionStr.includes('/')) {
+        expressionStr = handleMultiplicationDivision(expressionStr);
+        if(`${expressionStr}`.includes("Error")) return "Error: Are you trying to implode the universe? Dividing by zero is not allowed.";
+    }
+
+    // handle Addition and Subtraction (in order from left to right)
+    if((expressionStr.includes('+') || expressionStr.includes('~')) && !expressionStr.includes('e')) {
+        expressionStr = handleAdditionSubtraction(expressionStr);
+    }
+    
+    return Number(Number(expressionStr).toFixed(2));
+}
+
 /* ************************************ //
 //         OPERATION FUNCTIONS          //
 // ************************************ */
@@ -8,7 +47,7 @@ const operate = (operator, value1, value2) => {
         case "+":
             return a + b;
             break;
-        case "-":
+        case "~":
             return a - b;
             break;
         case "*":
@@ -18,7 +57,7 @@ const operate = (operator, value1, value2) => {
             if(b===0) {
                 console.log("Error: Are you trying to implode the universe? Dividing by zero is not allowed.");
                 return "Error";
-            } else return (a/b).toFixed(3);
+            } else return (a/b).toFixed(10);
             break;
         case "^":
             return Math.pow(a, b);
@@ -61,10 +100,10 @@ const handleParenthesis = (expression) => {
 
 const handleFactorials = (expression) => {
     let expressionStr = '';
-    // Validate that only digits, decimals, !, ^, *, /, +, and - are used in expression
-    if(expression.match(/[^-!\d\+\/\*\^\.]/g)) { // Invalid
+    // Validate that only digits, decimals, !, ^, *, /, ~, +, and - are used in expression
+    if(expression.match(/[^-!~\d\+\/\*\^\.]/g)) { // Invalid
         console.log(`Error: Invalid input in handleFactorials function: ${expression}`);
-    } else if(!expression.match(/[^!\d]/g)) { // If only digits/decimals and ! are present
+    } else if(!expression.match(/[^-!\d]/g)) { // If only digits/decimals and ! are present
         expressionStr = `${expression}`.replace(/[\d]+!/, (x) => {
             return `${x} `
         });
@@ -79,8 +118,8 @@ const handleFactorials = (expression) => {
             });
         expressionStr = inputArr.join('');
     } else {
-        // Store ^, *, /, +, - operators and replace them once we handle the !.
-        expressionStr = tempRemoveOperators(/[-\+\*\/\^]/g, expression);
+        // Store ^, *, /, +, ~ operators and replace them once we handle the !.
+        expressionStr = tempRemoveOperators(/[~\+\*\/\^]/g, expression);
     }
     // If another ! exists, recursively call handleFactorials
     if(`${expressionStr}`.includes('!')) {
@@ -92,13 +131,13 @@ const handleFactorials = (expression) => {
 
 const handleExponents = (expression) => {
     let expressionStr = '';
-    // Validate that only digits, decimals, ^, *, /, +, and - are used in expression
-    if(expression.match(/[^-\d\+\/\*\^\.]/g)) { // Invalid
+    // Validate that only digits, decimals, ^, *, /, ~, +, and - are used in expression
+    if(expression.match(/[^-~\d\+\/\*\^\.]/g)) { // Invalid
         console.log(`Error: Invalid input in handleExponents function: ${expression}`);
-    } else if(!expression.match(/[^\d\^\.]/g)) { // If only digits/decimals and ^ are present
+    } else if(!expression.match(/[^-\d\^\.]/g)) { // If only digits/decimals and ^ are present
         // Order of multiple consecutive powers is reversed so that if there are stacked exponents (power to a power), the top level exponent will be handled first: 2^3^4 will be handled as 2^(3^4).
 
-        //TODO: INSTEAD OF REVERSING, USE POP TO DO TOP LEVEL EXPONENTS FIRST
+        //TODO: INSTEAD OF REVERSING, TRY POP TO DO TOP LEVEL EXPONENTS FIRST
         expressionStr = `${expression}`.split('^').reverse().join('^').replace(/[\d]+\^[\d]+/, (x) => {
             return ` ${x} `
         });
@@ -113,8 +152,8 @@ const handleExponents = (expression) => {
             });
         expressionStr = inputArr.join('').split('^').reverse().join('^');
     } else {
-        // Store *, /, +, - operators and replace them once we handle the ^.
-        expressionStr = tempRemoveOperators(/[-\+\*\/]/g, expression);
+        // Store *, /, +, ~ operators and replace them once we handle the ^.
+        expressionStr = tempRemoveOperators(/[~\+\*\/]/g, expression);
     }
     // If another power exists, recursively call handleExponents
     if(`${expressionStr}`.includes('^')) {
@@ -126,10 +165,10 @@ const handleExponents = (expression) => {
 
 const handleMultiplicationDivision = (expression) => {
     let result = '';
-    // Validate that only digits, decimals, *, /, +, and - are used in expression
-    if(expression.match(/[^-\d\+\/\*\.]/g)) { //Invalid
+    // Validate that only digits, decimals, *, /, ~, +, and - are used in expression
+    if(expression.match(/[^-~\d\+\/\*\.]/g)) { //Invalid
         console.log(`Error: Invalid input in handleMultiplicationDivision function: ${expression}`);
-    } else if(!expression.match(/[^\d\/\*\.]/g)) { // If only digits/decimals and *, / are present
+    } else if(!expression.match(/[^-\d\/\*\.]/g)) { // If only digits/decimals and *, / are present
         // Multiplication and division are handled in order from left to right.
         let expressionStr = `${expression}`.replace(/[\*\/]/g, (x) => {
             return ` ${x} `;
@@ -142,20 +181,20 @@ const handleMultiplicationDivision = (expression) => {
             i++;
         }
     } else {
-        // Store +, - operators and replace them once we handle the * and /.
-        result = tempRemoveOperators(/[-\+]/g, expression);
+        // Store +, ~ operators and replace them once we handle the * and /.
+        result = tempRemoveOperators(/[~\+]/g, expression);
     }
     return `${result}`;
 }
 
 const handleAdditionSubtraction = (expression) => {
     let result = '';
-    // Validate that only digits, decimals, +, and - are used in expression
-    if(expression.match(/[^-\d\+\.]/g)) { //Invalid
+    // Validate that only digits, decimals, ~, +, and - are used in expression
+    if(expression.match(/[^-~\d\+\.]/g)) { //Invalid
         console.log(`Error: Invalid input in handleAdditionSubtraction function: ${expression}`);
     } else {
         // Addition and subtraction are handled in order from left to right.
-        let expressionStr = `${expression}`.replace(/[-\+]/g, (x) => {
+        let expressionStr = `${expression}`.replace(/[~\+]/g, (x) => {
             return ` ${x} `;
         });
         const inputArr = expressionStr.split(' ');
@@ -175,60 +214,51 @@ const handleAdditionSubtraction = (expression) => {
 const tempRemoveOperators = (untargetedOperRegex, expression) => {
     // Store untargeted operators so that we can replace them once we handle the target operator.
     const operatorArr = expression.match(untargetedOperRegex);
-    console.log(operatorArr);
     // Temporarily remove untargeted operators operators
     let expressionStr = `${expression}`.replace(untargetedOperRegex, ' ');
-    console.log(expressionStr);
     // Each element is now a digit/decimal or a series of target operators and digits to be evaluated
     const inputArr = expressionStr
         .split(' ')
         .map((element, index) => {
-            if(index<operatorArr.length) {
+            if(operatorArr && index<operatorArr.length) {
                 return `${calculate(element)}${operatorArr[index]}`;
             } else {
                 return `${calculate(element)}`;
             }
     });
-    console.log(inputArr);
     expressionStr = inputArr.join('');
-    console.log(expressionStr);
     return `${expressionStr}`;
 }
 
-/* ************************************ //
-//         CALCULATE FUNCTION           //
-// ************************************ */
+const implicitToExplicitMultiplication = (expression) => {
+    // Rewrite multiplication implied by parentheses with explicit * for parsing
+    expression = expression.replace(/[\d]+\(/g, (x) => {
+        return x.replace(/\(/, '*(');
+    });
+    expression = expression.replace(/\)[\d]+/g, (x) => {
+        return x.replace(/\)/, ')*');
+    });
+    expression = expression.replace(/\)\(/g, (x) => {
+        return x.replace(/\)/, ')*');
+    });
+    // Rewrite a factorial followed by a digit or ( as explicit multiplication
+    expression = expression.replace(/![\d]+/g, (x) => {
+        return x.replace(/!/, '!*');
+    });
+    expression = expression.replace(/!\(/g, (x) => {
+        return x.replace(/!/, '!*');
+    });
+    return expression;
+}
 
-const calculate = (expression) => {
-    let expressionStr = `${expression}`;
-
-    // handle Parentheses
-    if(expressionStr.includes('(')) {
-        expressionStr = handleParenthesis(expressionStr);
-    }
-
-    // handle Factorials
-    if(expressionStr.includes('!')) {
-        expressionStr = handleFactorials(expressionStr);
-    }
-
-    // handle Exponents
-    if(expressionStr.includes('^')) {
-        expressionStr = handleExponents(expressionStr);
-    }
-
-    // handle Multiplication and Division (in order from left to right)
-    if(expressionStr.includes('*') || expressionStr.includes('/')) {
-        expressionStr = handleMultiplicationDivision(expressionStr);
-        if(`${expressionStr}`.includes("Error")) return "Error: Are you trying to implode the universe? Dividing by zero is not allowed.";
-    }
-
-    // handle Addition and Subtraction (in order from left to right)
-    if((expressionStr.includes('+') || expressionStr.includes('-')) && !expressionStr.includes('e')) {
-        expressionStr = handleAdditionSubtraction(expressionStr);
-    }
-    
-    return Number(Number(expressionStr).toFixed(2));
+const prepNegatives = (expression) => {
+    // Rewrite subtraction as ~ to distinguish from negatives.
+    expression = expression
+        .replace(/\+-/g, '+0~')
+        .replace(/[\d\)]-[\d\(]/g, (x) => {
+            return x.replace(/-/, '~');
+        });
+    return expression;
 }
 
 /* ************************************ //
@@ -242,7 +272,7 @@ const isValidExpression = (expression) => {
 }
 
 const isValidInput = (expression) => {
-    const invalidInputs = /[^-%!\+\*\/\^\(\)\.\d]/g;
+    const invalidInputs = /[^-%!~\+\*\/\^\(\)\.\d]/g;
     const invalidArr = expression.match(invalidInputs);
     if(invalidArr) {
         return [false, "Error: Invalid entry. Please enter a valid mathematical expression."];
@@ -270,7 +300,7 @@ const isValidParenthesis = (expression) => {
 }
 
 const isValidOperators = (expression) => {
-    const operators = ["+", "-", "*", "/", "^", "%"];
+    const operators = ["+", "~", "*", "/", "^", "%"];
     const arr = expression.split('');
     for(let i=1; i<arr.length; i++) {
         // Cannot have two consecutive operators: "+/"
@@ -301,26 +331,6 @@ const isValidDecimals = (expression) => {
     return [true, ''];
 }
 
-const implicitToExplicitMultiplication = (expression) => {
-    // Rewrite multiplication implied by parentheses with explicit * for parsing
-    expression = expression.replace(/[\d]+\(/g, (x) => {
-        return x.replace(/\(/, '*(');
-    });
-    expression = expression.replace(/\)[\d]+/g, (x) => {
-        return x.replace(/\)/, ')*');
-    });
-    expression = expression.replace(/\)\(/g, (x) => {
-        return x.replace(/\)/, ')*');
-    });
-    // Rewrite a factorial followed by a digit or ( as explicit multiplication
-    expression = expression.replace(/![\d]+/g, (x) => {
-        return x.replace(/!/, '!*');
-    });
-    expression = expression.replace(/!\(/g, (x) => {
-        return x.replace(/!/, '!*');
-    });
-    return expression;
-}
 
 /* ************************************ //
 //        INPUT ENTRY FUNCTIONS         //
@@ -405,4 +415,3 @@ const addListeners = () => {
 
 addListeners();
 
-module.exports = calculate;
